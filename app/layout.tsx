4665b099bb119a5e0a64e2d1a9d854ce9b69fb94
Artyom Pratsko <kabanproff@gmail.com>
2024-08-30 06:05:46 +0300

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal?: React.ReactNode;
 }>) {
   return (
     <html lang="en">
